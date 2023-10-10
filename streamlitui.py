77266308dd5e14c595206f893834b5412075e7da
Owais Shaikh <122448767+TheOwaisShaikh@users.noreply.github.com>
2023-10-09 21:58:14 -0700

    Update streamlitui.py

--- streamlitui.py
+++ streamlitui.py
@@ -2,9 +2,9 @@ import os
 import tempfile
 import streamlit as st
 from streamlit_chat import message
-from webquery import WebQuery
+from webquery import PDFQuery, WebQuery
 
-st.set_page_config(page_title="Website to Chatbot")
+st.set_page_config(page_title="Chat Interface")
 
 def display_messages():
     st.subheader("Chat")
@@ -15,45 +15,67 @@ def display_messages():
 def process_input():
     if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
         user_text = st.session_state["user_input"].strip()
+
+        if st.session_state["mode"] == "PDF":
+            query_processor = st.session_state["pdfquery"]
+        else:
+            query_processor = st.session_state["webquery"]
+
         with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
-            query_text = st.session_state["webquery"].ask(user_text)
+            query_text = query_processor.ask(user_text)
 
         st.session_state["messages"].append((user_text, True))
         st.session_state["messages"].append((query_text, False))
-        
+
 def ingest_input():
     if st.session_state["input_url"] and len(st.session_state["input_url"].strip()) > 0:
         url = st.session_state["input_url"].strip()
         with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
-            ingest_text = st.session_state["webquery"].ingest(url)
+            st.session_state["webquery"].ingest(url)
+
+def read_and_save_file():
+    st.session_state["pdfquery"].forget()
+    st.session_state["messages"] = []
+    st.session_state["user_input"] = ""
+    for file in st.session_state["file_uploader"]:
+        with tempfile.NamedTemporaryFile(delete=False) as tf:
+            tf.write(file.getbuffer())
+            file_path = tf.name
+        with st.session_state["ingestion_spinner"], st.spinner(f"Ingesting {file.name}"):
+            st.session_state["pdfquery"].ingest(file_path)
+        os.remove(file_path)
 
 def is_openai_api_key_set() -> bool:
-    # Always return True since the key is hard-coded
-    return True
+    return len(st.session_state["OPENAI_API_KEY"]) > 0
 
 def main():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
-        st.session_state["url"] = ""
-        
-        # Hardcoded API key
-        st.session_state["OPENAI_API_KEY"] = "OPENAI_API_KEY"
-        st.session_state["webquery"] = WebQuery(st.session_state["OPENAI_API_KEY"])
+        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
+        if is_openai_api_key_set():
+            st.session_state["webquery"] = WebQuery(st.session_state["OPENAI_API_KEY"])
+            st.session_state["pdfquery"] = PDFQuery(st.session_state["OPENAI_API_KEY"])
+        else:
+            st.session_state["webquery"] = None
+            st.session_state["pdfquery"] = None
 
-    st.header("Website to Chatbot")
-    
-    # The input field for the API key has been commented out since it's hardcoded.
-    # st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password")
+    st.header("Chat Interface")
+    st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password")
 
-    st.subheader("Add a url")
-    st.text_input("Input url", value=st.session_state["url"], key="input_url", disabled=not is_openai_api_key_set(), on_change=ingest_input)
+    st.session_state["mode"] = st.radio("Mode", ["Website", "PDF"])
+
+    if st.session_state["mode"] == "Website":
+        st.subheader("Add a url")
+        st.text_input("Input url", key="input_url", disabled=not is_openai_api_key_set(), on_change=ingest_input)
+    else:
+        st.subheader("Upload a document")
+        st.file_uploader("Upload document", type=["pdf"], key="file_uploader", on_change=read_and_save_file, label_visibility="collapsed", accept_multiple_files=True, disabled=not is_openai_api_key_set())
+
     st.session_state["ingestion_spinner"] = st.empty()
-
     display_messages()
     st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
 
     st.divider()
-    st.markdown("Source code: [Github](https://github.com/TheOwaisShaikh/chatgptforeveryone)")
 
 if __name__ == "__main__":
     main()
