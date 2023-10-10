d8d75ce34529f902434946ecf470fe3b028b6ec4
Owais Shaikh <122448767+TheOwaisShaikh@users.noreply.github.com>
2023-10-09 21:59:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+from langchain.document_loaders import PyPDFLoader, UnstructuredPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain, BaseCombineDocumentsChain
 from langchain.tools import BaseTool
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from pydantic import Field
-from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain, BaseCombineDocumentsChain
-from langchain.chat_models import ChatOpenAI
-import os, asyncio, trafilatura
 from langchain.docstore.document import Document
+from pydantic import Field
+import os
+import trafilatura
 
 def _get_text_splitter():
     return RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size = 500,
-        chunk_overlap  = 20,
-        length_function = len,
+        chunk_size=500,
+        chunk_overlap=20,
+        length_function=len,
     )
 
 class WebpageQATool(BaseTool):
@@ -37,5 +41,17 @@ class WebpageQATool(BaseTool):
 
 llm = ChatOpenAI(temperature=1.0)        
 query_website_tool = WebpageQATool(qa_chain=load_qa_with_sources_chain(llm))
-url = "https://www.nbcnews.com/"
-print(query_website_tool.run("any news in trend?"))
+url = "www.altacademy.org"
+print(query_website_tool.run("what are there pricings"))
+
+# PDF-related code
+loader = UnstructuredPDFLoader("book.pdf")
+pages = loader.load_and_split()
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+
+query = "Who is Rich Dad?"
+docs = docsearch.get_relevant_documents(query)
+chain = load_qa_chain(ChatOpenAI(temperature=0), chain_type="stuff")
+output = chain.run(input_documents=docs, question=query)
+print(output)
